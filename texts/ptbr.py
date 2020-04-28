434105983049b6ac967be3ce176840933d185693
Leonardo Setubal <setubal43@gmail.com>
2020-04-28 16:04:23 -0300

    chore: texto

--- texts/ptbr.py
+++ texts/ptbr.py
@@ -7,8 +7,8 @@ saudacoes: List[str] = [
 ]
 
 quemsoueu = 'Eu sou Janet,' \
-			' a assistente desde grupo, ' \
-			'qualquer coisa que vocês precisarem podem me pedir, ' \
-			'oque eu não souber ' \
-			'ou fazer entre em contato com meu criador.' \
-			'Para saber mais sobres meus comandos use $help.'
+            ' a assistente desde grupo, ' \
+            'qualquer coisa que vocês precisarem podem me pedir, ' \
+            'oque eu não souber ' \
+            'ou fazer entre em contato com meu criador.' \
+            'Para saber mais sobres meus comandos use $help.'
