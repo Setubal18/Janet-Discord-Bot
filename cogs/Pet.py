0bbbf12665751922a8891b564c4adb47f75174b6
Leonardo Setubal <setubal43@gmail.com>
2020-05-07 22:27:39 -0300

    feat: mando foto de dog e cat, utilizando embed

--- cogs/Pet.py
+++ cogs/Pet.py
@@ -1,28 +1,43 @@
 # -*- coding: utf-8 -*-
-
 from discord.ext import commands
 
+from utils.embed import create_embed
 from utils.pets_urls import get_image_url
 
 
 class Pet(commands.Cog):
-	"""The description for Pet goes here."""
+	"""Manda foto de um cachorro ou de um gato"""
 
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.group(name='manda', aliases=['envia'])
+	@commands.group(name='manda', aliases=['envia', 'send'])
 	async def manda(self, ctx):
+		petsPerm = ['cats', 'dogs']
 		if ctx.subcommand_passed is None:
 			await ctx.send('Pode enviar junto com manda: dogs, cats')
-
-	# if ctx.subcommand_passed != 'dogs' or 'cats':
-	# 	await ctx.send('No momento só posso te enviar dogs ou cats')
+		elif ctx.subcommand_passed not in petsPerm:
+			await ctx.send('No momento só posso te enviar dogs ou cats')
 
 	@manda.command(name='dogs')
 	async def dogs(self, ctx):
-		url = get_image_url()
-		await ctx.send(f'Aqui está: {url}')
+		url = get_image_url(1)
+		embed = create_embed(
+			autorName='Dog',
+			url=url,
+			footer='Dog'
+		)
+		await ctx.send(embed=embed)
+
+	@manda.command(name='cats')
+	async def cats(self, ctx):
+		url = get_image_url(2)
+		embed = create_embed(
+			autorName='Cats',
+			url=url,
+			footer='Cats'
+		)
+		await ctx.send(embed=embed)
 
 
 def setup(bot):
--- utils/pets_urls.py
+++ utils/pets_urls.py
@@ -3,17 +3,23 @@ import re
 import requests
 
 
-def get_url():
+def get_url_dog():
 	contents = requests.get('https://random.dog/woof.json').json()
-	url = contents['url']
-	return url
+	return contents['url']
 
 
-def get_image_url():
+def get_url_cat():
+	content = requests.get('http://aws.random.cat/meow').json()
+	return content['file']
+
+
+def get_image_url(animal):
 	allowed_extension = ['jpg', 'jpeg', 'png', 'gif']
 	file_extension = ''
 	while file_extension not in allowed_extension:
-		url = get_url()
-		print('get_img_url', url)
+		if animal == 1:
+			url = get_url_dog()
+		elif animal == 2:
+			url = get_url_cat()
 		file_extension = re.search("([^.]*)$", url).group(1).lower()
 	return url
