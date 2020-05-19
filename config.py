5f17aaf0224e7afdf20f612ade791d23c60a8956
Leonardo Setubal <setubal43@gmail.com>
2020-05-19 17:16:06 -0300

    feat: Adicionado Requiriments.txt e config.py
    Signed-off-by: Leonardo Setubal <setubal43@gmail.com>

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path  # python3 only
+
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+token = os.getenv('TOKEN')
+url_janet_photo = os.getenv('URL_JANET_PHOTO')
+
+# cogs
+cogs = ['cogs.Greetings', 'cogs.Invite', 'cogs.Pet']
